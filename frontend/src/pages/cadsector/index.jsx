663b9ff3c34d28698030fb944127ada2c6e7cecc
rafael-p-s <rafaelsky321@gmail.com>
2024-04-22 01:23:51 -0300

    At the emoment screen CadSector is done.

--- frontend/src/pages/cadsector/index.jsx
+++ frontend/src/pages/cadsector/index.jsx
@@ -1,8 +1,79 @@
+import React from "react";
+//CSS:
+import "./index.css";
+//Components:
+import { NavBar } from "../../components/navbar";
+import { ButtonBtn } from "../../components/buttons/button";
+import { InputGeral } from "../../components/input";
+
 export function CadSector() {
-    return (
-      <>
-        <h1>CadSector</h1>
-      </>
-    );
-  }
-  
+  return (
+    <>
+      <NavBar>Cadastro Setor</NavBar>
+      <form action="" method="post">
+        <div className="div_cad_setor">
+          <InputGeral
+            label="Discrição:"
+            onchange={(e) => console.log(e.target.value)}
+            type="text"
+            placeholderText="Digite a Razão Social..."
+          />
+          <InputGeral
+            label="Empresa:"
+            onchange={(e) => console.log(e.target.value)}
+            type="text"
+            placeholderText="Busque a empresa..."
+          />
+        </div>
+        <div className="div_tabela_setor">
+          <table>
+            <thead>
+              <tr>
+                <th>Empresa</th>
+                <th>
+                  {/* <ButtonBtn>Deletar</ButtonBtn> */}
+                  Deletar
+                </th>
+              </tr>
+            </thead>
+          </table>
+        </div>
+        <div className="div_buttons">
+          <ButtonBtn
+            style={{
+              backgroundColor: "#F10404",
+              color: "#FFFFFF",
+              padding: "10px 20px",
+              border: "none",
+              borderRadius: "8px",
+              cursor: "pointer",
+              textDecoration: "none",
+              boxShadow: "2px 2px 5px rgba(0, 0, 0, 0.2)",
+              fontSize: "20px",
+              margin: "0px 5px",
+            }}
+            to="/"
+          >
+            Cancelar
+          </ButtonBtn>
+          <ButtonBtn
+            style={{
+              backgroundColor: "#2FD467",
+              color: "#FFFFFF",
+              padding: "10px 30px",
+              border: "none",
+              borderRadius: "8px",
+              cursor: "pointer",
+              textDecoration: "none",
+              boxShadow: "2px 2px 5px rgba(0, 0, 0, 0.2)",
+              fontSize: "20px",
+              margin: "0px 5px",
+            }}
+          >
+            Salvar
+          </ButtonBtn>
+        </div>
+      </form>
+    </>
+  );
+}
